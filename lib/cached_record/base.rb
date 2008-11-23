88eac9b7b15beeb65337b3c65273b0a374d8c824
comboy <kacper.ciesla@gmail.com>
2008-11-23 20:37:02 +0100

    testing framework almost done

--- lib/cached_record/base.rb
+++ lib/cached_record/base.rb
@@ -1,13 +1,15 @@
 module CachedRecord
   class Base < ::ActiveRecord::Base
-  end
 
-  def self.cache
-    @cache ||= ::CACHE
-  end
-  
-  def self.cache=(cache)
-    @cache = cache
+    def self.cache
+      @cache ||= ::CACHE
+    end
+
+    def self.cache=(cache)
+      @cache = cache
+    end
+
+    self.abstract_class = true
   end
   
 end
--- test/test_cached_record.rb
+++ test/test_cached_record.rb
@@ -3,8 +3,15 @@ require 'test/unit'
 
 class CachedRecordTest < Test::Unit::TestCase
   
+  include CachedRecord::TestHelper
+  
   def setup
     CachedRecord::TestEnvironment.setup
+  end
+  
+  def test_base
+   assert User.find_by_id(1)   
+   assert OriginalUser.find_by_id(1)
   end
   
   def test_something
@@ -14,4 +21,9 @@ class CachedRecordTest < Test::Unit::TestCase
   def test_blah
     assert true
   end
+  
+  def test_invalitading_on_save
+    assert true
+  end
 end
+
